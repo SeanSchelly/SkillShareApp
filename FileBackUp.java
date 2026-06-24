7185d566184ffa90377a3ec9099b8ed8a4c88e75
SeanSchelly <seanschellhas7@gmail.com>
2026-06-24 22:12:22 +0300

    minor removal

--- FileBackUp.java
+++ FileBackUp.java
@@ -1,5 +1,3 @@
-// [Integration] Removed package declaration to put all classes in default package, accessible from Main.java
-
 abstract class FileBackUp {
     abstract void writerBackUp(Profile [] profiles, Profile usrProfile, Person usrData);
 }
