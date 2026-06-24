b7d36e5ac713f1a3f502c92d0aa7ee9e38661e7e
SeanSchelly <seanschellhas7@gmail.com>
2026-06-24 21:29:59 +0300

    Minor change

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         requesthandlersys requestSystem = new requesthandlersys();
